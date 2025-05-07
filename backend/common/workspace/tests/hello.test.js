ebab6a180a39f5795badfd635d81c3cbd0266018
Rhys Cleary <rhysjcleary@gmail.com>
2025-05-07 22:36:13 +1000

    chore: testing ci/cd
    this tests the back-end systems automated build and test processes

--- backend/common/workspace/tests/hello.test.js
+++ backend/common/workspace/tests/hello.test.js
@@ -7,6 +7,7 @@ describe('Tests index', function () {
     it('verifies successful response', async () => {
         const event = {}
         const context = {};
+        const test = "hi"
 
         const result = await helloHandler(event, context)
 
