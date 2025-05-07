0575089d57da13cedccfc94e2d7497b56fb2cdd5
Rhys Cleary <rhysjcleary@gmail.com>
2025-05-07 21:34:20 +1000

    chore: file refactor
    This change restructures the backend file structure and sets up testing

---
+++ backend/common/workspace/tests/hello.test.js
@@ -0,0 +1,21 @@
+'use strict';
+
+//const { helloHandler } = require('../functions/hello.js');
+import { helloHandler } from '../functions/hello.js';
+
+describe('Tests index', function () {
+    it('verifies successful response', async () => {
+        const event = {}
+        const context = {};
+
+        const result = await helloHandler(event, context)
+
+        expect(result).toBeInstanceOf(Object);
+        expect(result.statusCode).toEqual(200);
+
+        let response = JSON.parse(result.body);
+
+        expect(response).toBeInstanceOf(Object);
+        expect(response.message).toEqual("hello world");
+    });
+});
---
+++ backend/jest.config.js
@@ -0,0 +1,9 @@
+/*module.exports = {
+    transform: {
+      "^.+\\.mjs$": "babel-jest",  // Use babel-jest to transform mjs files
+      "^.+\\.js$": "babel-jest",   // Use babel-jest for js files as well
+    },
+    moduleFileExtensions: ['js', 'mjs', 'json', 'node'],  // Allow both .js and .mjs
+    testEnvironment: 'node',  // Set test environment to node (needed for Lambda functions)
+  };*/
+  
