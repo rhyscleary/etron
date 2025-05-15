392b7262f1afe450352f61d610b437c3ef3e929e
Rhys Cleary <rhysjcleary@gmail.com>
2025-05-15 21:31:48 +1000

    feat: split the template files
    Split the template files for workspaces

--- backend/common/workspace/functions/create-workspace.js
+++ backend/common/workspace/functions/create-workspace.js
@@ -1,3 +1,10 @@
-export const workspaceHandler = async (event, context) => {
+const workspaceHandler = async (event, context) => {
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify({message: "hello world"})
+    };
 
+    return response;
 };
+
+module.exports = { workspaceHandler };
