19049060b32e4257aad8404d0288ea2055d46f0a
Rhys Cleary <rhysjcleary@gmail.com>
2025-05-19 00:48:47 +1000

    feat: create/update workspaces
    added the ability to add/create a workspace. Not fully tested yet

---
+++ backend/common/workspace/functions/workspace.js
@@ -0,0 +1,60 @@
+// Author: Rhys Cleary
+
+const {DynamoDBClient, PutCommand, GetCommand, DeleteCommand} = require('@aws-sdk/client-dynamodb')
+const {v4 : uuidv4} = require('uuid');
+const dynamoDB = new DynamoDBClient();
+const tableName = "Workspaces"
+
+const handler = async (event, context) => {
+    let statusCode = 200;
+    let body;
+    const headers = {
+        "Content-Type": "application/json"
+    };
+
+    try {
+        switch (event.routeKey) {
+            // add a workspace or update an existing one
+            case "PUT /workspace":
+                let requestJSON = JSON.parse(event.body);
+                let id = requestJSON.workspaceId || uuidv4();
+
+                await dynamoDB.send(
+                    new PutCommand( {
+                        TableName: tableName,
+                        Item: {
+                            workspaceId: id,
+                            name: requestJSON.name,
+                            location: requestJSON.location,
+                            ownerId: requestJSON.ownerId
+                        },
+                    })
+                );
+                body = "Workspace added or updated successfully ${id}";
+                break;
+
+            case "GET /workspace":
+                break;
+
+            case "DELETE /workspace/{id}":
+                break;
+            
+            default:
+                statusCode = 404;
+                body = "Unsupported route: ${event.routeKey}"
+                break;
+        }
+    } catch (error) {
+        console.error(error);
+        statusCode = 400;
+        body = error.message;
+    }
+
+    return {
+        statusCode,
+        headers,
+        body: JSON.stringify(body),
+    };
+};
+
+module.exports = { handler };
