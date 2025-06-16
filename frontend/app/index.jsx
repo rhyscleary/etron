6e3131e606687fcda1d512c63c175e244ca9f67f
BobFreak <noahbradley2118@gmail.com>
2025-06-16 19:55:29 +1000

    chore: cleaning up commented code

--- frontend/app/index.jsx
+++ frontend/app/index.jsx
@@ -4,48 +4,33 @@ import React from "react";
 import { Text, Button } from 'react-native';
 
 import { Amplify } from 'aws-amplify';
-import { withAuthenticator } from '@aws-amplify/ui-react-native';
+import { withAuthenticator, useAuthenticator } from '@aws-amplify/ui-react-native';
 import '@aws-amplify/ui-react/styles.css';
 import awsmobile from '../src/aws-exports';
 Amplify.configure(awsmobile);
 
-/*function App({ signOut, user }) {
-    return ( 
-        <>
-            <Text>Hello {user.signInDetails.loginId}</Text>
-            <Button onClick={signOut}>Sign out</Button>
-        </>
-    );
-}*/
+import { getCurrentUser } from 'aws-amplify/auth';
 
-function App({ signOut, user }) {
+async function currentAuthenticatedUser() {
+    try {
+        const { username, userId, signInDetails } = await getCurrentUser();
+        console.log(`username: ${username}`);
+        console.log(`userId: ${userId}`);
+        console.log(`email: ${signInDetails.loginId}`);
+        console.log(signInDetails);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+currentAuthenticatedUser();
+
+function App() {
     return ( 
         <>
             <Text>Hello</Text>
-            <Button title="Sign Out" onPress={signOut}>Sign out</Button>
         </>
     );
 }
 
 export default withAuthenticator(App);
-
-/*function App() {
-    return (
-        <Text>Testing</Text>
-    )
-}
-
-export default App;
-
-/*function firstScreen() {
-    const auth = useAuth();
-
-    return (
-        <div>
-            <p>First page</p>
-            <button onClick={() => auth.signinRedirect()}>Sign in</button>
-        </div>
-    );
-}
-
-export default firstScreen;*/
