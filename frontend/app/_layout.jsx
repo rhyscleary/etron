4747f4f6cadd5d403d9674019f0d3df4b8617c58
BobFreak <noahbradley2118@gmail.com>
2025-06-16 19:30:30 +1000

    feat: android amplify compatibility

--- frontend/app/_layout.jsx
+++ frontend/app/_layout.jsx
@@ -1,13 +1,4 @@
 import { Slot, Stack } from 'expo-router';
-import { AuthProvider } from "react-oidc-context";
-
-const cognitoAuthConfig = {
-    authority: "https://cognito-idp.ap-southeast-2.amazonaws.com/ap-southeast-2_m2sU1vTUd",
-    client_id: "30uq8adstk0l8blharsir4f6ed",
-    redirect_uri: "http://localhost:8081/logged-in",
-    response_type: "code",
-    scope: "email openid phone aws.cognito.signin.user.admin",
-};
 
 /*export default function RootLayout() {
     //return <Slot />;
@@ -15,8 +6,6 @@ const cognitoAuthConfig = {
 
 export default function Index() {
     return (
-        <AuthProvider {...cognitoAuthConfig}>
-            <Stack />
-        </AuthProvider>
+        <Stack />
     );
 }
--- frontend/app/index.jsx
+++ frontend/app/index.jsx
@@ -1,24 +1,41 @@
 import { Redirect, useRouter } from "expo-router";
 import React from "react";
-import { useAuth } from "react-oidc-context";
+
+import { Text, Button } from 'react-native';
 
 import { Amplify } from 'aws-amplify';
-
-import { withAuthenticator } from '@aws-amplify/ui-react';
+import { withAuthenticator } from '@aws-amplify/ui-react-native';
 import '@aws-amplify/ui-react/styles.css';
 import awsmobile from '../src/aws-exports';
 Amplify.configure(awsmobile);
 
-function App({ signOut, user }) {
-    return (
+/*function App({ signOut, user }) {
+    return ( 
         <>
-            <h1>Hello {user.signInDetails.loginId}</h1>
-            <button onClick={signOut}>Sign out</button>
+            <Text>Hello {user.signInDetails.loginId}</Text>
+            <Button onClick={signOut}>Sign out</Button>
+        </>
+    );
+}*/
+
+function App({ signOut, user }) {
+    return ( 
+        <>
+            <Text>Hello</Text>
+            <Button title="Sign Out" onPress={signOut}>Sign out</Button>
         </>
     );
 }
 
 export default withAuthenticator(App);
+
+/*function App() {
+    return (
+        <Text>Testing</Text>
+    )
+}
+
+export default App;
 
 /*function firstScreen() {
     const auth = useAuth();
