76c60c04c85f74a28b964d51d66f759ad4775a95
Rhys Cleary <rhysjcleary@gmail.com>
2025-06-23 03:55:22 +1000

    fix: tests
    Removed header test and added test providers

--- frontend/__tests__/components/Header.test.js
+++
@@ -1,9 +0,0 @@
-import { render } from "@testing-library/react-native";
-import Header from "../../components/layout/Header";
-
-/*describe("Header", () => {
-    it("render the correct title", () => {
-        const { getByText } = render(<Header title="Test Title" />);
-        expect(getByText("Test Title")).toBeTruthy();
-    })
-});*/
