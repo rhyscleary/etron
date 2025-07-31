aed0b686ab6b2d278ac73e5f373bea7445e80b3c
BobFreak <noahbradley2118@gmail.com>
2025-08-01 01:26:23 +1000

    feat: get profile photo from s3 and save in cache

--- frontend/app/(auth)/profile-photo.jsx
+++ frontend/app/(auth)/profile-photo.jsx
@@ -1,17 +1,55 @@
 import { Pressable, View, Button } from "react-native";
+import { useState, useEffect } from "react";
 import * as ImagePicker from "expo-image-picker";
 import { Text, Alert } from "react-native-paper";
 import { commonStyles } from "../../assets/styles/stylesheets/common";
 import * as FileSystem from "expo-file-system";
 import { Image } from 'expo-image';
-import { uploadData } from 'aws-amplify/storage';
+import { uploadData, getUrl } from 'aws-amplify/storage';
 import { Buffer } from 'buffer';
-import { updateUserAttribute, getCurrentUser } from 'aws-amplify/auth';
+import { updateUserAttribute, fetchUserAttributes, getCurrentUser } from 'aws-amplify/auth';
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 global.Buffer = global.Buffer || Buffer
 
 const changeProfilePhoto = () => {
+    const [profilePhotoUri, setProfilePhotoUri] = useState(null);
+
+    // Load the profile photo URL upon component mount
+    useEffect(() => {
+        const fetchProfilePhoto = async () => {
+            try {
+                // Load the photo from local storage if possible
+                console.log("Fetching from local storage...");
+                const cachedUri = await AsyncStorage.getItem('profilePhotoUri');
+                if (cachedUri) {
+                    setProfilePhotoUri(cachedUri);
+                    console.log("Profile photo loaded from cache.");
+                    return;
+                }
+
+                // If not in local storage, then get from S3
+                console.log("Fetching from S3...")
+                const userAttributes = await fetchUserAttributes();
+                const S3Url = userAttributes.picture;
+                if (!S3Url) {
+                    console.log("Profile photo URL not found.");
+                    return;
+                }
+                const S3UrlResult = await getUrl({path: S3Url});
+                await AsyncStorage.setItem('profilePhotoUri', s3UrlResult.url.toString())
+                setProfilePhotoUri(S3UrlResult.url.toString());
+                console.log("New profile photo URL fetched and cached.");
+            } catch (error) {
+                console.log("Profile photo URL fetch unsuccessful:", error);
+            }
+        }
+        fetchProfilePhoto();
+    }, [])
+
+    // Function for uploading photo to S3
     const handleUploadPhoto = async () => {
+        // Get photo library access permission
         const permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
         if (!permissionResult.granted) {
             console.log("Photo picking failed: Photo access was denied.");
@@ -19,10 +57,11 @@ const changeProfilePhoto = () => {
             return;
         }
 
+        // Open photo library and get user to pick photo
         const result = await ImagePicker.launchImageLibraryAsync({
             mediaTypes: 'images',
             allowsEditing: true,
-            //aspect: [1, 1]  //todo: check if this works, i read it but haven't tested it yet
+            //aspect: [1, 1],  //todo: check if this works, i read it but haven't tested it yet
             quality: 0.7,  //todo: find a lower number for this that fits the tiny profile photos so we don't waste storage
         });
         if (result.canceled) {
@@ -30,57 +69,64 @@ const changeProfilePhoto = () => {
             return;
         }
 
+        // Get the selected photo URI from the device 
         const asset = result.assets[0];
         const localUri = asset.uri;
-        console.log("Photo picked successfully:", localUri);
-
+        console.log("Local URI of photo obtained.");
+        
+        // Get the destination S3 path
         const { userId } = await getCurrentUser();
-        const fileName = `${userId}/${Date.now()}.jpg`;  //todo: get adding user's ID to the file name working; had trouble with it
-        const s3FilePath = `public/${fileName}`;
-        console.log("s3 File Path:", s3FilePath);
+        const fileName = `${userId}/${Date.now()}.jpg`;
+        const S3FilePath = `public/${fileName}`;
+        console.log("S3 file path created.")
 
+        // Upload the photo to S3
+        console.log("Uploading photo to S3 bucket...")
         try {
             const fileBuffer = await FileSystem.readAsStringAsync(localUri, {
                 encoding: FileSystem.EncodingType.Base64,
             });
 
             const result = await uploadData({
-                path: s3FilePath,
+                path: S3FilePath,
                 data: Buffer.from(fileBuffer, 'base64')
             }).result;
-            console.log("Successfully uploaded photo:", result);
+            console.log("Photo uploaded successfully.");
+            setProfilePhotoUri(localUri);
+            await AsyncStorage.setItem("profilePhotoUri", localUri);
+            console.log("Locally saved photo replaced.");
         } catch (error) {
             console.log("Error uploading photo:", error);
+            return;
         }
 
+        // Update the S3 path in the user's details
         try {
             const output = await updateUserAttribute({
                 userAttribute: {
                     attributeKey: 'picture',
-                    value: s3FilePath
+                    value: S3FilePath
                 }
             });
 
             const { nextStep } = output;
             if (nextStep.updateAttributeStep == "DONE") {
-                console.log("Profile picture URL updated successfully.");                
+                console.log("User details profile picture URL updated.");                
             } else {
-                console.log("Profile picture URL update not finished:", nextStep);
+                console.log("User details profile picture URL update not finished:", nextStep);
             }
         } catch (error) {
-            console.log("Error uploading profile picture URL to user:", error);
+            console.log("Profile picture URL upload to user details unsuccessful:", error);
+            return;
         }
-
-        //todo: add the s3FilePath to the user's picture attribute in Cognito
-        //todo: also add way to display the photo in the app
     }
 
     return (
         <View style={commonStyles.screen}>
-            <Text> Test </Text>
+            <Text> Test </Text> 
             <Button title="Choose and upload photo" onPress={handleUploadPhoto} />
             <Image
-                source={{ uri: 'https://picsum.photos/100' }}
+                source={{ uri:profilePhotoUri }}
                 style={{ width: 100, height: 100, borderRadius: 50 }}
                 placeholder = {"Profile photo goes here"}
             />
