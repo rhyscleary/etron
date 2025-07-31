3f6f72dff32ffe47dba562bc58f97dd92634507a
BobFreak <noahbradley2118@gmail.com>
2025-07-31 20:43:02 +1000

    feat: added saving s3 path to cognito profile, added placeholder photo display

--- frontend/app/(auth)/profile-photo.jsx
+++ frontend/app/(auth)/profile-photo.jsx
@@ -3,15 +3,14 @@ import * as ImagePicker from "expo-image-picker";
 import { Text, Alert } from "react-native-paper";
 import { commonStyles } from "../../assets/styles/stylesheets/common";
 import * as FileSystem from "expo-file-system";
-import { Storage } from "aws-amplify";
+import { Image } from 'expo-image';
 import { uploadData } from 'aws-amplify/storage';
 import { Buffer } from 'buffer';
+import { updateUserAttribute, getCurrentUser } from 'aws-amplify/auth';
 
 global.Buffer = global.Buffer || Buffer
 
 const changeProfilePhoto = () => {
-    console.log("Storage is:", Storage);
-
     const handleUploadPhoto = async () => {
         const permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
         if (!permissionResult.granted) {
@@ -34,10 +33,11 @@ const changeProfilePhoto = () => {
         const asset = result.assets[0];
         const localUri = asset.uri;
         console.log("Photo picked successfully:", localUri);
-        const fileName = `user-${Date.now()}.jpg`;  //todo: get adding user's ID to the file name working; had trouble with it
-        console.log("File name:", fileName);
-        const s3Key = `public/${fileName}`;
-        console.log("s3Key:", s3Key);
+
+        const { userId } = await getCurrentUser();
+        const fileName = `${userId}/${Date.now()}.jpg`;  //todo: get adding user's ID to the file name working; had trouble with it
+        const s3FilePath = `public/${fileName}`;
+        console.log("s3 File Path:", s3FilePath);
 
         try {
             const fileBuffer = await FileSystem.readAsStringAsync(localUri, {
@@ -45,7 +45,7 @@ const changeProfilePhoto = () => {
             });
 
             const result = await uploadData({
-                path: s3Key,
+                path: s3FilePath,
                 data: Buffer.from(fileBuffer, 'base64')
             }).result;
             console.log("Successfully uploaded photo:", result);
@@ -53,7 +53,25 @@ const changeProfilePhoto = () => {
             console.log("Error uploading photo:", error);
         }
 
-        //todo: add the s3Key to the user's picture attribute in Cognito
+        try {
+            const output = await updateUserAttribute({
+                userAttribute: {
+                    attributeKey: 'picture',
+                    value: s3FilePath
+                }
+            });
+
+            const { nextStep } = output;
+            if (nextStep.updateAttributeStep == "DONE") {
+                console.log("Profile picture URL updated successfully.");                
+            } else {
+                console.log("Profile picture URL update not finished:", nextStep);
+            }
+        } catch (error) {
+            console.log("Error uploading profile picture URL to user:", error);
+        }
+
+        //todo: add the s3FilePath to the user's picture attribute in Cognito
         //todo: also add way to display the photo in the app
     }
 
@@ -61,6 +79,11 @@ const changeProfilePhoto = () => {
         <View style={commonStyles.screen}>
             <Text> Test </Text>
             <Button title="Choose and upload photo" onPress={handleUploadPhoto} />
+            <Image
+                source={{ uri: 'https://picsum.photos/100' }}
+                style={{ width: 100, height: 100, borderRadius: 50 }}
+                placeholder = {"Profile photo goes here"}
+            />
         </View>
     )
 }
