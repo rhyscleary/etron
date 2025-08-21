0131c33ca21ff2a1e90e3070b667995f12467500
Rhys Cleary <rhysjcleary@gmail.com>
2025-08-22 09:49:31 +1000

    fix: add scope

--- frontend/amplify-backend/amplify/auth/resource.ts
+++ frontend/amplify-backend/amplify/auth/resource.ts
@@ -10,7 +10,11 @@ export const auth = defineAuth({
     externalProviders: {
       google: {
         clientId: secret('GOOGLE_CLIENT_ID'),
-        clientSecret: secret('GOOGLE_CLIENT_SECRET')
+        clientSecret: secret('GOOGLE_CLIENT_SECRET'),
+
+        attributeMapping: {
+          email: 'email'
+        }
       },
       callbackUrls: ["myapp://callback"],
       logoutUrls: ["myapp://signout/"],
