df725cfd2941dc432820eb9e257ba5dd2fd4b587
BobFreak <noahbradley2118@gmail.com>
2025-08-27 20:24:03 +1000

    fix: storage paths

--- frontend/amplify-backend/amplify/storage/resource.ts
+++ frontend/amplify-backend/amplify/storage/resource.ts
@@ -20,7 +20,7 @@ export const workspaceReadyDataStorage = defineStorage({
 export const usersStorage = defineStorage({
 	name: 'users',
 	access: (allow) => ({
-		'*': [
+		'/*': [
 			allow.authenticated.to(['read', 'write', 'delete'])  //TODO: Customise these permissions to be as limited as reasonable
 		]
 	})
@@ -29,7 +29,7 @@ export const usersStorage = defineStorage({
 export const workspacesStorage = defineStorage({
 	name: 'workspaces',
 	access: (allow) => ({
-		'*': [
+		'/*': [
 			allow.authenticated.to(['read', 'write', 'delete'])
 		]
 	})
