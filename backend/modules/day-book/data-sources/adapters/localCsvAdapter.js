ea51245c37296a36136f3b1b287d60c716bdd05f
Rhys Cleary <69389978+rhyscleary@users.noreply.github.com>
2025-09-04 23:45:24 +1000

    feat: fixed file upload data formatting

--- backend/modules/day-book/data-sources/adapters/localCsvAdapter.js
+++ backend/modules/day-book/data-sources/adapters/localCsvAdapter.js
@@ -1,17 +1,11 @@
 // Author(s): Rhys Cleary
 
-// validate the configurations before creating the data source
+// stub validation functions
 function validateConfig(config) {}
-
-// validate the secrets before creating the data source
 function validateSecrets(secrets, authType) {}
-
-// poll server
-async function poll(config, secrets) {}
 
 module.exports = {
     validateConfig,
     validateSecrets,
     supportsPolling: false,
-    poll
 };
