451015d60ce53e8269a7736d9f07b9d579cc3901
BobFreak <noahbradley2118@gmail.com>
2025-10-05 16:55:13 +1100

    feat: button to go between create and join workspace pages

--- frontend/app/(auth)/join-workspace.jsx
+++ frontend/app/(auth)/join-workspace.jsx
@@ -142,9 +142,13 @@ const JoinWorkspace = () => {
         }
     }
 
+    function navigateToCreateWorkspace() {
+        router.navigate("/(auth)/create-workspace");
+    }
+
     return (
         <View style={commonStyles.screen}>
-            <Header title="Join Workspace" showBack />
+            <Header title="Join Workspace" />
 
             <Text style={{ fontSize: 16, marginBottom: 12 }}>
               You have the following options:
@@ -181,6 +185,10 @@ const JoinWorkspace = () => {
                 )}
 
             </View>
+            <BasicButton
+                label={"Create Workspace"}
+                onPress={navigateToCreateWorkspace}
+            />
         </View>
     )
 
